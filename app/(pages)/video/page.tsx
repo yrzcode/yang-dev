f57eb99242778bb1e39da9fa74d4ec17c2e1196d
rz_yang <yrzcode@hotmail.com>
2024-05-23 21:51:37 +0900

    Add Video Card

--- app/(pages)/video/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function YoutubeCopyHome() {
-  return <div>YoutubeCopyHome</div>;
-}
--- app/layouts/Header/Profile/Profile.tsx
+++ app/layouts/Header/Profile/Profile.tsx
@@ -8,26 +8,30 @@ import {
   MenuGroup,
   MenuItem,
   MenuList,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
 /* Styles */
-import { createProfileStyles } from './styles';
+import { createProfileStyles } from "./styles";
 
 /* Profile */
 export const Profile = () => {
   const styles = createProfileStyles();
+
   return (
     <Box h="100%" w="100%">
       <Menu>
         <MenuButton as={Button} colorScheme="pink">
           Profile
         </MenuButton>
+
         <MenuList color="red" {...styles.menuList}>
           <MenuGroup title="Profile">
             <MenuItem {...styles.menuItem}>My Account</MenuItem>
             <MenuItem {...styles.menuItem}>Payments </MenuItem>
           </MenuGroup>
+
           <MenuDivider />
+
           <MenuGroup title="Help">
             <MenuItem {...styles.menuItem}>Docs</MenuItem>
             <MenuItem {...styles.menuItem}>FAQ</MenuItem>
